1d634c35c780f4a5990d941d80d5ec62059f77d2
Thomas LE MIGNAN <tle-mign@student.42.fr>
2014-01-11 18:43:44 +0100

    Create cpu_clock_init.c

---
+++ cpu_clock_init.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cpu_clock_init.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tle-mign <tle-mign@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/01/11 14:00:01 by tle-mign          #+#    #+#             */
+/*   Updated: 2014/01/11 15:08:56 by tle-mign         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libspacetime.h"
+
+unsigned long		ft_cpu_clock_init(void)
+{
+	clock_t		start;
+
+	start = clock();
+	return ((unsigned long)start);
+}
